1eeee476db2f8a5785fb1be7fada00e467fda4f3
AlanSquid <squid78313@gmail.com>
2022-10-17 01:42:56 +0800

    feat: check account function

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const User = require('./models/user')
+const e = require('express')
 
 const app = express()
 require('./config/mongoose')
@@ -9,8 +12,42 @@ app.set('view engine', 'hbs')
 
 app.use(express.static('public'))
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body
+  if (!email || !password) {
+    return res.render('index', { result: '請輸入帳號密碼' })
+  }
+  User.findOne({ email, password })
+    .lean()
+    .then(user => {
+      if (!user) {
+        res.render('index', { result: '帳號或密碼錯誤，請重新輸入' })
+      } else {
+        res.render('success', { firstName: user.firstName })
+      }
+    })
+
+  // 帳號驗證作法二:
+  // if (!email || !password) {
+  //   return res.render('index', { result: '請輸入帳號密碼' })
+  // }
+  // User.findOne({ email })
+  //   .lean()
+  //   .then(user => user ? user : res.render('index', { result: '查無此帳號，請先進行註冊' }))
+  //   .then(user => {
+  //     if (user.password !== password) {
+  //       res.render('index', { result: '密碼錯誤!' })
+  //     } else {
+  //       res.render('success', { firstName: user.firstName })
+  //     }
+  //   })
+  //   .catch(err => console.log(err))
 })
 
 app.listen(3000, () => {
