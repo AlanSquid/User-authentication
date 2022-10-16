3d0525329a2ca1e4957b22099bf258ccb37e3d6d
AlanSquid <squid78313@gmail.com>
2022-10-16 13:05:02 +0800

    built index page UI and CSS

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express()
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.render('index')
 })
